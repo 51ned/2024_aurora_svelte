486b4b605c7f090651be24d1113d77c447a7e47e
Denis <sustain@bk.ru>
2024-06-29 15:46:55 +0300

    it's magic

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,8 +4,14 @@ import type { LayoutLoad } from './$types'
 export const load: LayoutLoad = async ({ data }) => {
   let { gtmId, scheme, vw } = data
 
-  if (!scheme && typeof window !== 'undefined') {
-    scheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  if (typeof window !== 'undefined') {
+    if (!scheme) {
+      scheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+
+    if (!vw) {
+      vw = String(window.innerWidth)
+    }
   }
 
   return { gtmId, scheme, vw }
