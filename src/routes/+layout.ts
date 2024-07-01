dd880cd59cef09bec5ea31f3d199955966e971c6
Denis <sustain@bk.ru>
2024-07-01 21:44:08 +0300

    ...

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,15 +2,15 @@ import type { LayoutLoad } from './$types'
 
 
 export const load: LayoutLoad = async ({ data }) => {
-  console.log(data.scheme)
-  if (data.scheme !== null && data.scheme !== undefined) {
+  console.log(`cheme from headers: ${data.scheme}`)
+  if (data.scheme !== null) {
     return data
   }
 
   if (typeof window !== 'undefined') {
     data.scheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
     data.vw = String(window.innerWidth)
-    
+    console.log(`cheme from window: ${data.scheme}`)
     return data
   }
 }
