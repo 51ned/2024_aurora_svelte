04771be90f912e1db88a0d534589f48d46838d59
Denis <sustain@bk.ru>
2024-07-02 13:25:46 +0300

    ...

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,9 +2,9 @@ import type { Handle } from '@sveltejs/kit'
 
 
 export const handle: Handle = async ({ event, resolve }) => {
-  // event.setHeaders({
-  //   'accept-ch': 'sec-ch-prefers-color-scheme, sec-ch-viewport-width'
-  // })
+  event.setHeaders({
+    'accept-ch': 'sec-ch-prefers-color-scheme, sec-ch-viewport-width'
+  })
   
   return await resolve(event)
 }
