804569622c79f6d8d09020c069d223dd37356261
Denis <sustain@bk.ru>
2024-07-02 10:34:14 +0300

    ...

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,8 +2,11 @@ import type { Handle } from '@sveltejs/kit'
 
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const response = await resolve(event)
-  response.headers.set('accept-ch', 'sec-ch-prefers-color-scheme')
+	const response = await resolve(event, {
+    transformPageChunk: ({ html }) => html
+  })
+
+  response.headers.set('accept-ch', 'sec-ch-prefers-color-scheme, sec-ch-viewport-width')
   
   return response
 }
