a22a3f3cdcca7c962a290f43bff4664fc3b2a9f8
Denis <sustain@bk.ru>
2024-07-02 13:34:43 +0300

    ...

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,10 @@
-import type { Handle } from '@sveltejs/kit'
+// import type { Handle } from '@sveltejs/kit'
 
 
-export const handle: Handle = async ({ event, resolve }) => {
-  event.setHeaders({
-    'X-Kak-Ya-Zaebalsya': 'Silno-Pizdos'
-  })
+// export const handle: Handle = async ({ event, resolve }) => {
+//   event.setHeaders({
+//     'X-Kak-Ya-Zaebalsya': 'Silno-Pizdos'
+//   })
   
-  return await resolve(event)
-}
+//   return await resolve(event)
+// }
