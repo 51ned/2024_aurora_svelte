24f21ce47220bd52c22c7c9e39d1f6b7326381df
Denis <sustain@bk.ru>
2024-07-02 12:41:26 +0300

    ...

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,9 +2,9 @@ import type { Handle } from '@sveltejs/kit'
 
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const response = await resolve(event)
-  console.log(response.headers)
-  response.headers.append('accept-ch', 'sec-ch-prefers-color-scheme, sec-ch-viewport-width')
-  console.log(response.headers)
-  return response
+  event.setHeaders({
+    'accept-ch': 'sec-ch-prefers-color-scheme, sec-ch-viewport-width'
+  })
+  
+  return await resolve(event)
 }
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,7 @@
 import type { LayoutServerLoad } from './$types'
 
 
-const load: LayoutServerLoad = async ({ request, setHeaders }) => {  
-  setHeaders({
-    'accept-ch': 'sec-ch-prefers-color-scheme, sec-ch-viewport-width'
-  })
-  
+const load: LayoutServerLoad = async ({ request }) => {  
   return {
     gtmId: import.meta.env.VITE_GTM_ID,
     scheme: request.headers.get('sec-ch-prefers-color-scheme'),
