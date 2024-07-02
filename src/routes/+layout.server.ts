052437c845d93d71b0e423bdec33865e3872717e
Denis <sustain@bk.ru>
2024-07-02 13:23:07 +0300

    ..

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,15 +1,15 @@
 import type { LayoutServerLoad } from './$types'
 
 
-const load: LayoutServerLoad = async ({ request, setHeaders }) => { 
+const load: LayoutServerLoad = async ({ setHeaders }) => { 
   setHeaders({
     'accept-ch': 'sec-ch-prefers-color-scheme, sec-ch-viewport-width'
   }) 
 
   return {
-    gtmId: import.meta.env.VITE_GTM_ID,
-    scheme: request.headers.get('sec-ch-prefers-color-scheme'),
-    vw: request.headers.get('sec-ch-viewport-width')
+    // gtmId: import.meta.env.VITE_GTM_ID,
+    // scheme: request.headers.get('sec-ch-prefers-color-scheme'),
+    // vw: request.headers.get('sec-ch-viewport-width')
   }
 }
 
