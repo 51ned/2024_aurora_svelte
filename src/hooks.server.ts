caf848100c49c9b6530f2246509f2bf6537b90dc
51ned <sustain@bk.ru>
2024-07-08 18:56:51 +0300

    experiments with hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,10 @@
+import { setContext } from 'svelte'
+import { sequence } from '@sveltejs/kit/hooks'
+
 import type { Handle } from '@sveltejs/kit'
 
 
-export const handle: Handle = async ({ event, resolve }) => {
+const setHeaders: Handle = async ({ event, resolve }) => {
 	// event.setHeaders({
   //   'accept-ch': 'sec-ch-prefers-color-scheme, sec-ch-viewport-width'
   // })
@@ -10,4 +13,26 @@ export const handle: Handle = async ({ event, resolve }) => {
   response.headers.set('accept-ch', 'sec-ch-prefers-color-scheme, sec-ch-viewport-width')
 
 	return response
+
+  // one way, or another - nevermind...
 }
+
+const getCookies: Handle = async ({ event, resolve }) => {
+	const cookieStore = event.cookies.getAll()
+	console.log(cookieStore)
+  
+	return resolve(event)
+}
+
+const getHeaders: Handle = async ({ event, resolve }) => {
+	const scheme = event.request.headers.get('sec-ch-prefers-color-scheme')
+	const vw = event.request.headers.get('sec-ch-viewport-width')
+  
+  console.log(`Scheme from hook: ${scheme}, VW from hook: ${vw}`)
+	// setContext here... Or on client side? Hmmm...
+	
+	return await resolve(event)
+}
+
+
+export const handle: Handle = sequence(getCookies, setHeaders, getHeaders)
