df0a0809bd37e0424267ee5f0928b5918a8db484
51ned <sustain@bk.ru>
2024-07-09 18:54:30 +0300

    ...

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,10 +3,11 @@ import type { Handle } from '@sveltejs/kit'
 
 
 const setHeaders: Handle = async ({ event, resolve }) => {
-  const response = await resolve(event)
-  response.headers.set('accept-ch', 'sec-ch-prefers-color-scheme, sec-ch-viewport-width')
-	console.log(response.headers)
-	return response
+  event.setHeaders({
+    'accept-ch': 'sec-ch-prefers-color-scheme, sec-ch-viewport-width'
+  })
+
+	return await resolve (event)
 }
 
 const getScheme: Handle = async ({ event, resolve }) => {
@@ -18,14 +19,11 @@ const getScheme: Handle = async ({ event, resolve }) => {
 		event.locals.scheme = event.request.headers.get('sec-ch-prefers-color-scheme')
 	}
 
-	console.log(`scheme from hooks: ${event.locals.scheme}`)
 	return await resolve(event)
 }
 
 const getVw: Handle = async ({ event, resolve }) => {
 	event.locals.vw = event.request.headers.get('sec-ch-viewport-width')
-
-	console.log(`vw from hooks: ${event.locals.vw}`)
 	return await resolve(event)
 }
 
